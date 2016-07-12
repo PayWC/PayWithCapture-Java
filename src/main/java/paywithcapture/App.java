bcd3deb8c52c4270a02b2e70f52aaf657a4ff3b0
Ridwan Olalere <ridwan@Ridwans-MacBook-Pro.local>
2016-07-12 16:38:54 +0100

    first commit

---
+++ src/main/java/paywithcapture/App.java
@@ -0,0 +1,13 @@
+package paywithcapture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
